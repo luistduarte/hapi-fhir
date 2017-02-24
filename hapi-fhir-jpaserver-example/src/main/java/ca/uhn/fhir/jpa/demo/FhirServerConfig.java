4643edcf81fcdc5286186358b8ce9bb9d62cddf7
luistduarte <lduarte.suil@gmail.com>
2017-02-24 12:49:48 +0000

    interceptor added on hapifhir-jpaserver-example

--- hapi-fhir-jpaserver-example/src/main/java/ca/uhn/fhir/jpa/demo/FhirServerConfig.java
+++ hapi-fhir-jpaserver-example/src/main/java/ca/uhn/fhir/jpa/demo/FhirServerConfig.java
@@ -45,7 +45,7 @@ public class FhirServerConfig extends BaseJavaConfigDstu2 {
 	/**
 	 * The following bean configures the database connection. The 'url' property value of "jdbc:derby:directory:jpaserver_derby_files;create=true" indicates that the server should save resources in a
 	 * directory called "jpaserver_derby_files".
-	 * 
+	 *
 	 * A URL to a remote database could also be placed here, along with login credentials and other properties supported by BasicDataSource.
 	 */
 	@Bean(destroyMethod = "close")
@@ -110,8 +110,11 @@ public class FhirServerConfig extends BaseJavaConfigDstu2 {
 
 	@Bean(autowire = Autowire.BY_TYPE)
 	public IServerInterceptor subscriptionSecurityInterceptor() {
-		SubscriptionsRequireManualActivationInterceptorDstu2 retVal = new SubscriptionsRequireManualActivationInterceptorDstu2();
-		return retVal;
+		//SubscriptionsRequireManualActivationInterceptorDstu2 retVal = new SubscriptionsRequireManualActivationInterceptorDstu2();
+		//return retVal;
+		BasicSecurityInterceptor inter = new BasicSecurityInterceptor();
+		return inter;
+
 	}
 
 	@Bean()
